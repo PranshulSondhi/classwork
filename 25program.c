5bac17912633d1462ff7f37f79636e373ae494e7
PranshulSondhi <122594767+PranshulSondhi@users.noreply.github.com>
2023-02-17 11:20:16 +0530

    Add files via upload

---
+++ 25program.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,a,s,b,m,w;
+    printf("The number is:");
+    scanf("%d",&n);
+    m=n;
+    w=n;
+    b=0;
+    while(n!=0){
+        n=n/10;
+        b=b+1;
+    }
+    s=0;
+    while(w!=0){
+        a=w%10;
+        s=s+pow(a,b);
+        w=w/10;
+    }
+    if(m==s){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
